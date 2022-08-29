bd8801d585c5b486536f29c8ba4dfa059812e60d
Max Saravia Moreira <saravia.max.stk2@gmail.com>
2022-08-29 01:28:14 -0300

    refactor: Accordion
    refactor was made in order to move variants and transitions to styled components declarations

--- frontend/src/components/Accordion/Accordion.tsx
+++ frontend/src/components/Accordion/Accordion.tsx
@@ -1,43 +1,27 @@
-import { useState } from 'react'
-import { AnimatePresence, motion, Variants } from 'framer-motion'
-import { MotionHeader } from './AccordionStyle'
+import React, { useState } from 'react'
+import { AnimatePresence } from 'framer-motion'
+import { MotionHeader, MotionContent } from './AccordionStyle'
+import { AccordionProps } from '../ts.reference'
 
-const variants: Variants = {
-  animate: { height: 'fit-content', opacity: 1, scale: 1 },
-  exit: { height: 0, opacity: 0, scale: 0 },
-  initial: { height: 0, opacity: 0, scale: 0 }
-}
-
-const Accordion = () => {
+const Accordion:React.FC<AccordionProps> = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   return (
-    <div>
-          <>
+    <>
       <MotionHeader
-        initial={false}
         animate={isOpen ? 'active' : 'inactive'}
         onClick={() => setIsOpen(prev => !prev)}
         >
-      <p>FAQs title</p>
+      <p>{title}</p>
       </MotionHeader>
       <AnimatePresence initial={false}>
         {isOpen && (
-          <motion.section
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            variants={variants}
-            transition={{ duration: 0.3 }}
+          <MotionContent
           >
-            FAQs response
-            Economicis es tu aplicacion de finanzas personales donde podes clasificar sus ingresos y gastos por categorias. Podras ver un resumen de tu flujo de dinero actual de manera simple y sencilla.
-
-Completamente gratuita, no contiene publicidad, ni anuncios de terceros y proximamente graficos historicos de tus movimientos!!
-          </motion.section>
+            {content}
+          </MotionContent>
         )}
       </AnimatePresence>
     </>
-    </div>
   )
 }
 export default Accordion
--- frontend/src/components/Accordion/AccordionStyle.tsx
+++ frontend/src/components/Accordion/AccordionStyle.tsx
@@ -2,6 +2,14 @@ import styled from 'styled-components'
 import { motion, Transition, Variants } from 'framer-motion'
 
 /* -------------------------------------------------------------------------- */
+/*                              ACCORDION'S STYLE                             */
+/* -------------------------------------------------------------------------- */
+
+export const AccordionStyle = styled.div`
+  width: 100%;
+`
+
+/* -------------------------------------------------------------------------- */
 /*                             ACCORDION'S HEADER                             */
 /* -------------------------------------------------------------------------- */
 
@@ -9,8 +17,24 @@ const headerVariant = {
   active: { backgroundColor: '#0055FF' },
   inactive: { backgroundColor: '#FF0088' }
 }
+
+export const MotionHeader = styled(motion.div).attrs(({ initial }) => ({
+  initial: initial || false,
+  variants: headerVariant
+}))`
+    display: flex;
+    width: 30rem;
+    padding: 0 1rem;
+    p{
+      line-height: 2rem;
+    }
+`
+/* -------------------------------------------------------------------------- */
+/*                             ACCORDION'S CONTENT                            */
+/* -------------------------------------------------------------------------- */
+
 const contentVariant: Variants = {
-  animate: { height: 'fit-content', opacity: 1, scale: 1 },
+  animate: { height: 'auto', opacity: 1, scale: 1 },
   exit: { height: 0, opacity: 0, scale: 0 },
   initial: { height: 0, opacity: 0, scale: 0 }
 }
@@ -18,21 +42,14 @@ const contentVariant: Variants = {
 const contentTransition: Transition = {
   duration: 0.3
 }
+
 export const MotionContent = styled(motion.div).attrs(({ initial }) => ({
-  initial,
+  animate: initial || 'animate',
+  exit: 'exit',
+  initial: 'initial',
   transition: contentTransition,
   variants: contentVariant
 }))`
     display: flex;
+    width: 30rem;
 `
-
-export const MotionHeader = styled(motion.div).attrs(({ initial }) => ({
-  initial,
-  variants: headerVariant
-}))`
-    display: flex;
-`
-
-/* -------------------------------------------------------------------------- */
-/*                             ACCORDION'S CONTENT                            */
-/* -------------------------------------------------------------------------- */
